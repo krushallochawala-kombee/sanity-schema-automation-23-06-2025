b2fd8a75ced1077f8822e0a1b96a5765d8551e74
krushallochawala-kombee <krushal.lochawala@kombee.com>
2025-07-26 10:13:23 +0530

    refactor: remove deprecated schema types and documents for a cleaner structure

--- schemaTypes/documents/page.ts
+++
@@ -1,9 +0,0 @@
-import {defineType, defineField} from 'sanity'
-export default defineType({
-    name: 'page', title: 'Page', type: 'document',
-    fields: [
-        defineField({name: 'title', title: 'Title', type: 'string'}),
-        defineField({name: 'slug', title: 'Slug', type: 'slug', options: {source: 'title'}}),
-        defineField({name: 'pageBuilder', title: 'Page Builder', type: 'array', of: [{type: 'heroHeaderSection'}, {type: 'socialProofSection'}, {type: 'quoteSection'}, {type: 'featuresSection'}, {type: 'metricsSection'}, {type: 'ctaSection'}, {type: 'footer'}] })
-    ]
-})
